b25429012e233adf2a4f56a94acd409a60d114ce
AndreDC <a24andredc@iesantonlosada.gal>
2025-05-15 15:48:24 +0200

    feat:  nuevo componente HomeButton

---
+++ components/ui/HomeButton.tsx
@@ -0,0 +1,39 @@
+import { Ionicons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
+import React from "react";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
+
+export default function HomeButton() {
+  const router = useRouter();
+
+  const handleNavigation = (button: string) => {
+    router.push("/home");
+  };
+
+  return (
+    <View>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => handleNavigation("RETURN")}
+      >
+        <Ionicons name="home-outline" size={20} color="white" />
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    paddingHorizontal: 15,
+    padding: 10,
+    borderRadius: 20,
+    backgroundColor: "#007AFF",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  homeButtonText: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "white",
+  },
+});
